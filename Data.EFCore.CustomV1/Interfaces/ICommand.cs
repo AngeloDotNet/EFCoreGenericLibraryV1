1dd5545bdb8498fceaee63c5d38ea234db3d2c17
Angelo Pirola <angelo9871@gmail.com>
2023-01-31 22:19:43 +0100

    Clean solution

--- Data.EFCore.CustomV1/Interfaces/ICommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Data.EFCore.CustomV1.Interfaces;
-
-public interface ICommand<T> where T : class
-{
-    Task CreateAsync(T entity);
-    Task UpdateAsync(T entity);
-    Task DeleteAsync(T entity);
-}
--- Data.EFCore.CustomV1/Interfaces/IDatabase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Data.EFCore.CustomV1.Interfaces;
-
-public interface IDatabase<T> where T : class
-{
-    Task<List<T>> GetAllAsync();
-    Task<T> GetByIdAsync(int id);
-}
